1a7526bb92599d301ebd7a33e9eb2f3d12498da7
circular-code <stephanos@malikotsinas.de>
2018-09-25 21:37:34 +0200

    added some basic styling

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ window.onload = () => {
             var barcodeDetector = new BarcodeDetector();
             barcodeDetector.detect(e.target)
                 .then(barcodes => {
-                barcodes.forEach(barcode => document.getElementById('result').textContent += (barcode.rawValue));
+                barcodes.forEach(barcode => document.getElementById('result').textContent = (barcode.rawValue));
                 })
                 .catch((e) => {
                 console.error("BarcodeDetection failed: " + e);
